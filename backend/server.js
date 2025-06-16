15df4c2ded495108fee4bf0a9b4608513c1b4ad0
jinglebells2020 <es6559@nyu.edu>
2025-06-16 22:00:59 +0800

    Remove deprecated mongoose connect options

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use('/api/resources', require('./routes/resources'));
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
